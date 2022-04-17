10c840f0d00d49ac73dbcdd7817512986897bba9
Aman Ostwal <ostwalaman7@gmail.com>
2022-04-17 16:07:53 +0530

    Variety of Vegetables Added

--- lib/data/home_page_data.dart
+++ lib/data/home_page_data.dart
@@ -3,50 +3,68 @@ import 'package:e_commerce/models/SingleProductModel.dart';
 List<SingleProductModel> singleProductData = [
   SingleProductModel(
     productImage:
-        'https://bigbasket.com/media/uploads/p/l/10000066_25-fresho-cabbage.jpg',
+        'https://www.bigbasket.com/media/uploads/p/l/10000331_13-fresho-onion-sambhar.jpg',
+    productName: 'Onions',
+    productPrice: 70,
+  ),
+  SingleProductModel(
+    productImage:
+        'https://www.bigbasket.com/media/uploads/p/l/20002674_4-fresho-asparagus.jpg',
+    productName: 'Asparagus',
+    productPrice: 70,
+  ),
+  SingleProductModel(
+    productImage:
+        'https://www.bigbasket.com/media/uploads/p/l/10000323-2_13-fresho-capsicum-yellow.jpg',
+    productName: 'Yellow Capsicum',
+    productPrice: 70,
+  ),
+  SingleProductModel(
+    productImage:
+        'https://www.bigbasket.com/media/uploads/p/l/40084052_4-fresho-capsicum-green-premium-institutional.jpg',
+    productName: 'Green Capsicum',
+    productPrice: 70,
+  ),
+  SingleProductModel(
+    productImage:
+        'https://www.bigbasket.com/media/uploads/p/l/40149660_1-fresho-capsicum-red.jpg',
+    productName: 'Red Capsicum',
+    productPrice: 70,
+  ),
+  SingleProductModel(
+    productImage:
+        'https://www.ugaoo.com/knowledge-center/wp-content/uploads/2016/10/shutterstock_384935137.jpg',
+    productName: 'Parsley',
+    productPrice: 70,
+  ),
+  SingleProductModel(
+    productImage:
+        'https://www.ugaoo.com/knowledge-center/wp-content/uploads/2016/10/shutterstock_279244529.jpg',
     productName: 'Cabbage',
     productPrice: 70,
   ),
   SingleProductModel(
     productImage:
-        'http://bigbasket.com/media/uploads/p/l/10000085-2_1-fresho-chinese-cabbage.jpg',
-    productName: 'Cabbage',
+        'https://www.ugaoo.com/knowledge-center/wp-content/uploads/2016/10/shutterstock_245736580.jpg',
+    productName: 'Red Cabbage',
     productPrice: 70,
   ),
   SingleProductModel(
     productImage:
-        'https://bigbasket.com/media/uploads/p/l/10000066_25-fresho-cabbage.jpg',
-    productName: 'Cabbage',
+        'https://www.bigbasket.com/media/uploads/p/l/20000981_6-fresho-spring-onion-with-roots.jpg',
+    productName: 'Spring Onion',
     productPrice: 70,
   ),
   SingleProductModel(
     productImage:
-        'http://bigbasket.com/media/uploads/p/l/10000085-2_1-fresho-chinese-cabbage.jpg',
-    productName: 'Cabbage',
+        'https://www.bigbasket.com/media/uploads/p/l/40202912_1-fresho-pumpkin-disco.jpg',
+    productName: 'Pumpkin',
     productPrice: 70,
   ),
   SingleProductModel(
     productImage:
-        'https://bigbasket.com/media/uploads/p/l/10000066_25-fresho-cabbage.jpg',
-    productName: 'Cabbage',
-    productPrice: 70,
-  ),
-  SingleProductModel(
-    productImage:
-        'http://bigbasket.com/media/uploads/p/l/10000085-2_1-fresho-chinese-cabbage.jpg',
-    productName: 'Cabbage',
-    productPrice: 70,
-  ),
-  SingleProductModel(
-    productImage:
-        'https://bigbasket.com/media/uploads/p/l/10000066_25-fresho-cabbage.jpg',
-    productName: 'Cabbage',
-    productPrice: 70,
-  ),
-  SingleProductModel(
-    productImage:
-        'http://bigbasket.com/media/uploads/p/l/10000085-2_1-fresho-chinese-cabbage.jpg',
-    productName: 'Cabbage',
+        'https://www.bigbasket.com/media/uploads/p/l/40018523_4-fresho-cucumber-english.jpg',
+    productName: 'Cucumber',
     productPrice: 70,
   ),
 ];
